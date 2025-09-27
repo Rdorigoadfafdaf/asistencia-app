0d3f6222115e1e4abb2572d5c5ac63f995375405
Rdorigoadfafdaf <rrrodrigohs@gmail.com>
2025-09-27 10:41:56 -0500

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import pytz
 
 # 🔹 Conexión con Google Sheets usando Secrets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -22,11 +23,13 @@ tipo = st.selectbox("🕒 Tipo de registro", ["Ingreso", "Salida"])
 
 if st.button("✅ Registrar asistencia"):
     if nombre.strip() != "":
-        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        tz = pytz.timezone("America/Lima")
+        fecha = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
         sheet.append_row([nombre, tipo, fecha])
         st.success(f"Asistencia registrada para {nombre} - {tipo} a las {fecha}")
     else:
         st.error("⚠️ Debes ingresar un nombre")
+
 
 
 
